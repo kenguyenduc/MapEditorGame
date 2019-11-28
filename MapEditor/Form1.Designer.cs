2167ceb01d36beef8aa3c4e442fd4bb7c6d34028
kenguyenduc <32872158+kenguyenduc@users.noreply.github.com>
2019-11-28 23:06:04 +0700

    the last time

---
+++ MapEditor/Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace MapEditor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnBrowseFilepath = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.btnBrowseOutput = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHeight = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtFileName = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.numSplitRow = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numSplitRow)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(93, 27);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.Size = new System.Drawing.Size(438, 20);
+            this.txtFilePath.TabIndex = 5;
+            // 
+            // btnBrowseFilepath
+            // 
+            this.btnBrowseFilepath.Location = new System.Drawing.Point(552, 27);
+            this.btnBrowseFilepath.Name = "btnBrowseFilepath";
+            this.btnBrowseFilepath.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseFilepath.TabIndex = 1;
+            this.btnBrowseFilepath.Text = "Browse...";
+            this.btnBrowseFilepath.UseVisualStyleBackColor = true;
+            this.btnBrowseFilepath.Click += new System.EventHandler(this.btnBrowseFilepath_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "File Path:";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(93, 76);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(438, 20);
+            this.txtOutput.TabIndex = 6;
+            // 
+            // btnBrowseOutput
+            // 
+            this.btnBrowseOutput.Location = new System.Drawing.Point(552, 74);
+            this.btnBrowseOutput.Name = "btnBrowseOutput";
+            this.btnBrowseOutput.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowseOutput.TabIndex = 2;
+            this.btnBrowseOutput.Text = "Browse...";
+            this.btnBrowseOutput.UseVisualStyleBackColor = true;
+            this.btnBrowseOutput.Click += new System.EventHandler(this.btnBrowseOutput_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Output :";
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(93, 127);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(52, 20);
+            this.txtWidth.TabIndex = 7;
+            this.txtWidth.Text = "16";
+            this.txtWidth.TextChanged += new System.EventHandler(this.txtWidth_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Width :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(28, 13);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "pixel";
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(384, 127);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(111, 61);
+            this.btnExport.TabIndex = 3;
+            this.btnExport.Text = "Export";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(212, 127);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Height :";
+            // 
+            // txtHeight
+            // 
+            this.txtHeight.Location = new System.Drawing.Point(262, 127);
+            this.txtHeight.Name = "txtHeight";
+            this.txtHeight.Size = new System.Drawing.Size(52, 20);
+            this.txtHeight.TabIndex = 8;
+            this.txtHeight.Text = "16";
+            this.txtHeight.TextChanged += new System.EventHandler(this.txtHeight_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(320, 127);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(28, 13);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "pixel";
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(516, 127);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(111, 61);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtFileName
+            // 
+            this.txtFileName.Location = new System.Drawing.Point(93, 165);
+            this.txtFileName.Name = "txtFileName";
+            this.txtFileName.Size = new System.Drawing.Size(243, 20);
+            this.txtFileName.TabIndex = 8;
+            this.txtFileName.Text = "result";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(20, 168);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(67, 13);
+            this.label7.TabIndex = 2;
+            this.label7.Text = "Save name :";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(20, 212);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(55, 13);
+            this.label8.TabIndex = 2;
+            this.label8.Text = "Split rows:";
+            // 
+            // numSplitRow
+            // 
+            this.numSplitRow.Location = new System.Drawing.Point(93, 212);
+            this.numSplitRow.Maximum = new decimal(new int[] {
+            16,
+            0,
+            0,
+            0});
+            this.numSplitRow.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numSplitRow.Name = "numSplitRow";
+            this.numSplitRow.Size = new System.Drawing.Size(52, 20);
+            this.numSplitRow.TabIndex = 9;
+            this.numSplitRow.Value = new decimal(new int[] {
+            4,
+            0,
+            0,
+            0});
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(642, 244);
+            this.Controls.Add(this.numSplitRow);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnBrowseOutput);
+            this.Controls.Add(this.txtFileName);
+            this.Controls.Add(this.txtHeight);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.btnBrowseFilepath);
+            this.Controls.Add(this.txtFilePath);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "My Application";
+            ((System.ComponentModel.ISupportInitialize)(this.numSplitRow)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnBrowseFilepath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Button btnBrowseOutput;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtHeight;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtFileName;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.NumericUpDown numSplitRow;
+    }
+}
+
---
+++ MapEditor/Form1.cs
@@ -0,0 +1,253 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Windows.Forms;
+
+namespace MapEditor
+{
+    public partial class frmMain : Form
+    {
+        public Dictionary<int, Bitmap> lstTileSet = new Dictionary<int, Bitmap>();
+        public static string enviroment = System.Environment.CurrentDirectory;
+        public int _pixel;
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowseFilepath_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog()
+            {
+                FileName = "Select a image file",
+                Filter = "Image Files (*.bmp;*.jpg;*.jpeg,*.png)|*.BMP;*.JPG;*.JPEG;*.PNG",
+                Title = "Open Image File..."
+            };
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    StreamReader streamReader = new StreamReader(openFileDialog.FileName);
+                    string fileName = openFileDialog.FileName;
+                    txtFilePath.Text = System.IO.Path.GetFullPath(fileName);
+                }
+                catch (SecurityException ex)
+                {
+                    MessageBox.Show($"Security error.\n\nError message: {ex.Message}\n\n" +
+                    $"Details:\n\n{ex.StackTrace}");
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnBrowseOutput_Click(object sender, EventArgs e)
+        {
+            //OpenFileDialog folderBrowser = new OpenFileDialog();
+            //// Set validate names and check file exists to false otherwise windows will
+            //// not let you select "Folder Selection."
+            //folderBrowser.ValidateNames = false;
+            //folderBrowser.CheckFileExists = false;
+            //folderBrowser.CheckPathExists = true;
+            //// Always default to Folder Selection.
+            //folderBrowser.FileName = "Folder Selection.";
+            //if (folderBrowser.ShowDialog() == DialogResult.OK)
+            //{
+            //    txtOutput.Text = Path.GetDirectoryName(folderBrowser.FileName);
+
+            //}
+            FolderBrowserDialog folderBrowser = new FolderBrowserDialog();
+            folderBrowser.Description = "Select a folder";
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                txtOutput.Text = folderBrowser.SelectedPath;
+            }
+        }
+
+        private void txtWidth_TextChanged(object sender, EventArgs e)
+        {
+            this.txtHeight.Text = this.txtWidth.Text;
+        }
+        private void txtHeight_TextChanged(object sender, EventArgs e)
+        {
+            this.txtWidth.Text = this.txtHeight.Text;
+        }
+
+        #region handle bitmap background
+        public bool IsEqual(Bitmap bmp1, Bitmap bmp2)
+        {
+            if (object.Equals(bmp1, bmp2))
+                return true;
+
+            int bytes = bmp1.Width * bmp1.Height * (Image.GetPixelFormatSize(bmp1.PixelFormat) / 8) - 1;
+
+            bool result = true;
+            var b1bytes = new byte[bytes];
+            var b2bytes = new byte[bytes];
+
+            var bitmapData1 = bmp1.LockBits(new Rectangle(0, 0, bmp1.Width, bmp1.Height), ImageLockMode.ReadOnly, bmp1.PixelFormat);
+            var bitmapData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), ImageLockMode.ReadOnly, bmp2.PixelFormat);
+
+            Marshal.Copy(bitmapData1.Scan0, b1bytes, 0, bytes);
+            Marshal.Copy(bitmapData2.Scan0, b2bytes, 0, bytes);
+
+            for (int n = 0; n < bytes; ++n)
+            {
+                if (b1bytes[n] != b2bytes[n])
+                {
+                    result = false;
+                    break;
+                }
+            }
+
+            bmp1.UnlockBits(bitmapData1);
+            bmp2.UnlockBits(bitmapData2);
+
+            return result;
+        }
+        public int AlreadyIncluded(Dictionary<int, Bitmap> source, Bitmap bitmap)
+        {
+            if (source.Count < 1)
+            {
+                return -1;
+            }
+            foreach (var b in source)
+            {
+                if (IsEqual(b.Value, bitmap))
+                {
+                    return b.Key;
+                }
+            }
+            return -1;
+        }
+
+        public Bitmap CropTile(Bitmap source, Rectangle sourceRect)
+        {
+            // An empty bitmap which will hold the cropped image
+            Bitmap bmp = new Bitmap(sourceRect.Width, sourceRect.Height);
+            Rectangle destRect = new Rectangle(0, 0, _pixel, _pixel);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            {
+                // Draw the given area (section) of the source image
+                // at location 0,0 on the empty bitmap (bmp)
+                g.DrawImage(source, destRect, sourceRect, GraphicsUnit.Pixel);
+            }
+
+            return bmp;
+        }
+
+        #endregion
+
+        #region Create Tile From Image
+        public void CreateTileFromPicture(string filePath)
+        {
+            //filePath = Path.Combine(projectDirectory, filePath);
+            if (!File.Exists(filePath))
+            {
+                Console.WriteLine("File Path Invalid");
+                return;
+            }
+
+            if (!filePath.EndsWith(".png") && !filePath.EndsWith(".PNG"))
+            {
+                Console.WriteLine("File is not an image");
+                return;
+            }
+            lstTileSet.Clear();
+            //Minimize
+            int index = 0;
+            int res = -1;
+            Bitmap img = new Bitmap(filePath);
+            int numTilesWidth = img.Width / _pixel;
+            int numTilesHeight = img.Height / _pixel;
+            string strTile = numTilesWidth + " " + numTilesHeight + "\n";
+            List<Bitmap> lstTiles = new List<Bitmap>();
+            Bitmap bitmapTile;
+            //cut image into species
+            for (int row = 0; row < numTilesHeight; row++)
+            {
+                for (int col = 0; col < numTilesWidth; col++)
+                {
+                    Rectangle sourceRect = new Rectangle(_pixel * col, _pixel * row, _pixel, _pixel);
+                    lstTiles.Add(CropTile(img, sourceRect));
+                    bitmapTile = CropTile(img, sourceRect);
+                    res = AlreadyIncluded(lstTileSet, bitmapTile);
+                    if (res == -1)
+                    {
+                        lstTileSet.Add(index, bitmapTile);
+                        strTile = strTile + index + " ";
+                        index++;
+                    }
+                    else
+                    {
+                        strTile = strTile + res + " ";
+                    }
+                }
+                strTile += "\n";
+            }
+            if (lstTiles.Count <= 0)
+            {
+                Console.WriteLine("Could no cut into tiles");
+                return;
+            }
+            //end write to file
+            StreamWriter streamWriter = new StreamWriter(Path.Combine(txtOutput.Text, txtFileName.Text + ".txt"));
+            streamWriter.Write(strTile);
+            streamWriter.Close();
+            //draw to image
+            //calculate szie of image
+            //Bitmap finalImg = new Bitmap(_pixel * lstTileSet.Count, _pixel);
+            //using (Graphics g = Graphics.FromImage(finalImg))
+            //{
+            //    g.Clear(Color.Black);
+            //    for (int i = 0; i < lstTileSet.Count; i++)
+            //    {
+            //        g.DrawImage(lstTileSet[i], new Rectangle(i * _pixel, 0, _pixel, _pixel), new Rectangle(0, 0, _pixel, _pixel), GraphicsUnit.Pixel);
+            //    }
+            //}
+            //finalImg.Save(Path.Combine(txtOutput.Text, txtFileName.Text + ".png"));
+
+            int _count = 0;
+            int _splitRows =Convert.ToInt32(numSplitRow.Value);
+            int _cols = lstTileSet.Count / _splitRows;
+            int _rows = _splitRows;
+            Bitmap finalImg = new Bitmap(_pixel * _cols, _pixel * _splitRows);
+            Pen pen = new Pen(Color.SeaGreen);
+            using (Graphics graphic = Graphics.FromImage(finalImg))
+            {
+                graphic.Clear(Color.Transparent);
+                for (int i = 0; i < _rows; i++)
+                {
+                    for (int j = 0; j < _cols; j++)
+                    {
+                        graphic.DrawImage(lstTileSet[_count], new Rectangle(j * _pixel, i * _pixel, _pixel, _pixel), new Rectangle(0, 0, _pixel, _pixel), GraphicsUnit.Pixel);
+                        _count++;
+                    }
+                }
+            }
+            finalImg.Save(Path.Combine(txtOutput.Text, txtFileName.Text + ".png"));
+        }
+        #endregion
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            _pixel = Int32.Parse(txtHeight.Text);
+            string filename = @"" + txtFilePath.Text;
+            CreateTileFromPicture(filename);
+            DialogResult dialogResult = MessageBox.Show("Done !!!", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            if (dialogResult == DialogResult.OK)
+            {
+                //txtFilePath.Text = txtOutput.Text = "";
+            }
+        }
+    }
+}
